04e30be46d0c600195c8a5aa0b0fa03d81998eea
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-01-20 23:46:43 +0700

    Image

--- src/app/loggedin/loggedin.component.ts
+++ src/app/loggedin/loggedin.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-loggedin',
   templateUrl: './loggedin.component.html',
-  styleUrls: ['./loggedin.component.scss']
+  styleUrls: ['./loggedin.component.scss'],
 })
 export class LoggedinComponent implements OnInit {
-  payload = {}
+  payload: Params = {};
+  avatar = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {
-  }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     const payload = this.activatedRoute.snapshot.queryParams['payload'];
     console.log(this.activatedRoute.snapshot.queryParams['payload']);
-    this.payload = JSON.parse(payload); 
+    if (payload) {
+      this.payload = JSON.parse(payload);
+      this.avatar = this.payload['user'].avatar;
+    }
     console.log(this.payload);
   }
-
-
 }
